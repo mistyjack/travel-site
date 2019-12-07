10af73b26e86d4a10b7c0e678dde79be15367f3b
mistyjack <oladepomustafa@gmail.com>
2019-12-07 17:46:22 +0100

    Corrected stickyHeader to StickyHeader

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,7 +2,7 @@ import '../styles/styles.css'
 import 'lazysizes'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
-import StickyHeader from './modules/stickyHeader'
+import StickyHeader from './modules/StickyHeader'
 
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
